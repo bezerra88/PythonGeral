7a60d2bc0392304b56039353c50ac767d794e092
CamilaB <112370441+cbezerras@users.noreply.github.com>
2023-09-06 15:33:00 -0300

    Create ExemplosPrints.py

---
+++ ExemplosPrints.py
@@ -0,0 +1,12 @@
+A=2.5678
+B=5
+pi=3.14
+print('A contém ',A, ' e B contém ',B)
+
+print("Pi =  %.2f"  %pi  + " area" )
+
+print("{} love {}!!".format("Maria",
+                              "Cats"))
+print("A={:.2f}".format(A))
+
+print(f'Este número com duas casas decimais: {A:.2f}')
